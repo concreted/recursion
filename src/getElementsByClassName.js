c4f8abcad40db91dacbc283d1ba0611d62929569
concreted <arichuang@gmail.com>
2014-07-25 12:17:56 -0700

    Remove console.log calls, slight refactor

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,33 +5,19 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
-    // your code here
-    /*
-    console.log(document.body);
-    console.log(document.body.childNodes);
-    console.log(document.body.classList);
-    */
+
     var recursivelyFindIn = function(nodes) {
 	if (nodes.length === 0) {
 	    return [];
 	}
 
-	var head = nodes[0];
-	//console.log(head);
-	//console.log(nodes);
-	var result = head.className === undefined ? [] : head.className.indexOf(className) >= 0 ? [head] : [];
-
-	var tail = nodes.slice(1, nodes.length);
-	tail = tail.concat(Array.prototype.slice.call(head.childNodes));
-
-
-	//console.log(nodes);
-	//console.log(Array.prototype.slice.call(head.childNodes));
-	//console.log(tail);
+	var headNode = nodes[0];
+	var head = headNode.className !== undefined && headNode.className.indexOf(className) >= 0 ? [headNode] : [];
+	var tail = nodes.slice(1, nodes.length).concat(Array.prototype.slice.call(headNode.childNodes));;
 	
-	return result.concat(recursivelyFindIn(tail));	
+	return head.concat(recursivelyFindIn(tail));	
     }
 
-    return recursivelyFindIn([document.body]);
+    return recursivelyFindIn([document]);
 
 };
